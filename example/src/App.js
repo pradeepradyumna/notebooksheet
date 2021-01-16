9ce953c60cb736b746727695e56306a23b1878b5
Pradeep Pradyumna <pradeepradyumna@gmail.com>
2021-01-16 14:50:06 +0530

    Updated styles

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
-
-import { ExampleComponent } from 'notebooksheet'
+import { Notebook } from 'notebooksheet'
 import 'notebooksheet/dist/index.css'
-
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const items = []
+  // Example items
+  // items[0] = 'one'
+  // items[1] = 'two'
+
+  const heading = '' // Enter a heading
+
+  return <Notebook heading={heading} items={items} />
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const Notebook = ({ heading, items }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.notebook}>
+        <h4 className={styles.notebookp}>{heading}</h4>
+        <ul>
+          {items.map((item) => (
+            <li key={Math.random() * 1000}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
